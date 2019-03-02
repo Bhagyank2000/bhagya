8bb71358d3e4f2f941e48de263562c318aeee7a7
Bhagyank2000 <48047578+Bhagyank2000@users.noreply.github.com>
2019-03-02 11:42:53 +0530

    Add files via upload

---
+++ bhagyadice8.py
@@ -0,0 +1,13 @@
+import random
+
+d=0
+p=0
+snl={8:37,13:34,38:9,11:2,25:4,40:68,52:81,65:46,89:70,93:64,76:97}
+
+def rolldice():
+
+   return random.randint(1,6)
+
+   while true:
+   	 	r=("press r to roll, q to quit : ")
+   	 	if r == 'r':
